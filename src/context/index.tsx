357bf4d42c1854a8ec79b082f04252521459f551
ufkaya <ufuk.kaya@autoricardo.ch>
2020-02-19 14:58:32 +0100

    fix: update state with ldClient

--- src/context/index.tsx
+++ src/context/index.tsx
@@ -48,11 +48,13 @@ const ProviderWithState: FC<ProviderProps> = ({
   useEffect(() => {
     if (ldUser) {
       const ldClient = initLDClient(ldClientId, ldUser, flags)
-      if (!flags) {
-        ldClient.on("initialized", () => {
+      ldClient.on("initialized", () => {
+        if (!flags) {
           setState({ ldClient, flags: ldClient.allFlags() })
-        })
-      }
+        } else {
+          setState({ ldClient })
+        }
+      })
 
       ldClient.on("change", (changes: LDFlagChangeset) => {
         const flattened: LDFlagSet = {}
@@ -65,8 +67,7 @@ const ProviderWithState: FC<ProviderProps> = ({
 
         const mergedFlags = { ...state.flags, ...flattened }
         setState({
-          flags: mergedFlags,
-          ldClient
+          flags: mergedFlags
         })
       })
     }
