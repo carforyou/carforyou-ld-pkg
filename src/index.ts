0ef1b2da0b269b7db7db28afefeed96ba847ac6e
Christian Felder (masone) <ema@rh-productions.ch>
2020-07-26 13:12:52 +0200

    remove HOC

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import { LDProvider, LDContext } from "./context/index"
 import { useFlags } from "./hooks/index"
-import { withLD, WithLDProps } from "./withLD"
 import { LDUser, LDData } from "./types"
 
-export { LDProvider, LDContext, withLD, WithLDProps, useFlags, LDUser, LDData }
+export { LDProvider, LDContext, useFlags, LDUser, LDData }
