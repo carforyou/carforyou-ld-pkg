1d52a4ed242fb6f3139e4b82652670e57d9841fa
Krzysztof Sakwerda <ksakwerda@gmail.com>
2020-08-05 16:56:00 +0200

    fix: try defensive

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -2,6 +2,6 @@ import { useContext } from "react"
 import { LDContext } from "../context/index"
 
 export const useFlags = () => {
-  const { flags } = useContext(LDContext)
-  return flags
+  const data = useContext(LDContext)
+  return data?.flags || {}
 }
