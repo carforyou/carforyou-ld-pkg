e5d0888aca5da97e7a6b96e78b3e0f18e31ee85e
Lorenzo Kappeler <me@lnk.codes>
2021-04-23 14:02:09 +0200

    tests: Add tests for cient initialization behaviour

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   testPathIgnorePatterns: ["<rootDir>/pkg/"],
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-  testMatch: ["**/__tests__/**/*.test.(t|j)s"],
+  testMatch: ["**/__tests__/**/*.test.(t|j)s?(x)"],
+  transform: {
+    "\\.tsx?$": "ts-jest",
+  },
 }
